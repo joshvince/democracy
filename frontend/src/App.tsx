47e67c809e8d5867b883df75c952776b2d976405
Josh Vince <joshmvince@gmail.com>
2021-02-16 20:04:24 +0000

    add frontend scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="font-normal font-serif">
+      <h1 className="font-black text-8xl">Democracy 2.0</h1>
+    </div>
+  )
+}
+
+export default App
